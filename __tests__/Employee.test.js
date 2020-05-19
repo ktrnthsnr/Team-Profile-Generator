ca26221fcc430963e8d5a98fa359d5aa9a2df0d3
ktrnthsnr <asakthisner@hotmail.com>
2020-05-19 04:20:55 -0700

    added Employee test and constructor

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+// Employee test constructor (Important: constructors are Capitalized!)
+const Employee = require('../lib/Employee.js');
+    // console.log(Employee);
+
+test("validates name is a string", () => {
+    // create a new employee with a name, ID, and email values
+    const employee = new Employee('Helen',2,'helen@hotmail.com');
+    expect(employee.name).toBe('Helen');
+    // checks the name value is a string
+    expect(typeof employee.name).toBe("string");
+    //verifies id is a number
+    expect(employee.id).toEqual(expect.any(Number));
+});
+
+
+
+
+
+
+// run test with a $ npm test   
